545d30703e69f2501ed71095190bb2d50f75e0bf
akm0055 <aarav28071999@gmail.com>
2023-12-06 00:27:39 +0530

    added comments

--- pascalTriangle.c
+++ pascalTriangle.c
@@ -1,53 +1,37 @@
 #include <stdio.h>
 
+// function to calculate factorial
 int factorial(int n) {
-    int fact = 1;
-    for (int i = 1; i <= n; i++) {
-        fact *= i;
+    int result = 1;
+    for (int i = 2; i <= n; i++) {
+        result *= i;
     }
-    return fact;
-}
-
-void printPascalTriangle(int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= i; j++) {
-            printf("%d ", factorial(i) / (factorial(j) * factorial(i - j)));
-        }
-        printf("\n");
-    }
+    return result;
 }
 
 int main() {
     int n;
     printf("Enter the number of rows for Pascal's Triangle: ");
     scanf("%d", &n);
-    /*
-    ----1
-    ---1 1
-    --1 2 1
-    -1 3 3 1
-    1 4 6 4 1
 
-    */
-
-
-
-
-
+    // iterate over the rows
     for (int i = 0; i < n; i++)
     {
-      for( int k =n-i-1 ; k>0; k--)
-      {
-        printf(" ");
-      }
-      for(int j = 0 ; j<=i; j++)
-      {
-        printf("%d ",factorial(i)/(factorial(j)*factorial(i-j)));
-      }
-      printf("\n");
+        // print the leading spaces
+        for( int k =n-i-1 ; k>0; k--)
+        {
+            printf(" ");
+        }
+        
+        // calculate and print the values of each row
+        for(int j = 0 ; j<=i; j++)
+        {
+            printf("%d ",factorial(i)/(factorial(j)*factorial(i-j)));
+        }
+        
+        // print a newline character
+        printf("\n");
     }
     
-
-    // printPascalTriangle(n);
     return 0;
 }
